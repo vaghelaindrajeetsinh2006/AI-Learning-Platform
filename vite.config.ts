ebf1ecaf93568e41463c909cf62d8a1648d170a7
Vaghela Indrajeetsinh <vaghelaindrajeetsinh2006@gmail.com>
2026-08-24 21:52:44 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/AI-Learning-Platform/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
